b4259860a68cfad1e4f5b98d7bb59059851420d0
blin4504 <bl4504@g.rit.edu>
2022-12-26 01:48:57 -0500

    finish countries

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const Country = ({country}) => {
+  return (
+  <>
+    <h1>{country.name.common}</h1>
+    <div>capital {country.capital}</div>
+    <div>area {country.area}</div>
+    <h3>languages:</h3>
+    <ul>
+      {Object.entries(country.languages).map(([key, value]) => 
+        <li key={key}>{value}</li>
+      )}
+    </ul>
+    <img src={country.flags.png} />
+  </>)
+}
+
+const Countries = ({filtered, setFiltered}) => {
+  if (filtered.length === 1) return null
+  return (
+  <>
+    {filtered.map(country => 
+          <div key={country.name.common}>{country.name.common} <button onClick={() => setFiltered([country])}>show</button></div>)}
+  </>
+  )
+}
 
 function App() {
+  const [search, setSearch] = useState('')
+  const [filtered, setFiltered] = useState([])
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    axios.get(`https://restcountries.com/v3.1/all`)
+    .then(response => {
+    setCountries(response.data)
+    setFiltered(response.data)
+    })
+  }, [])
+
+  const filterSearch = (event) => {
+    const searched = event.target.value
+    setSearch(searched)
+    setFiltered(countries.filter(country => country.name.common.toLowerCase().includes(searched)))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+    <div>find countries <input value={search} onChange={filterSearch}></input></div>
+      {filtered.length === 1 ? <Country country={filtered[0]} /> : null}
+      {filtered.length > 10 ? <div>Too many matches, specify another filter</div> : 
+      <Countries filtered={filtered} setFiltered={setFiltered} />}
+  </>
+  )
 }
 
 export default App;
