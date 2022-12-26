f8da81566db79f307c187924bef72ee487151007
blin4504 <bl4504@g.rit.edu>
2022-12-25 21:35:42 -0500

    add server to phonebook

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,20 +1,25 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456'},
-    { name: 'Ada Lovelace', number: '39-44-5323523'},
-    { name: 'Dan Abramov', number: '12-43-234345'},
-    { name: 'Mary Poppendieck', number: '39-23-6423122'}
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
-  const [personToShow, setPersonToShow] = useState([...persons])
+  const [personToShow, setPersonToShow] = useState([])
+
+  const hook = () => {
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+      setPersonToShow(response.data)
+    })
+  }
+
+  useEffect(hook, [])
 
   const handleNewPerson = (event) => {
     console.log(event.target.value)
@@ -43,7 +48,7 @@ const App = () => {
   }
 
   const filterPersons = (event) => {
-    const search = event.target.value
+    const search = event.target.value.toLowerCase()
     setFilter(search)
     setPersonToShow(
       persons.filter((person) => person.name.toLowerCase().includes(search))
