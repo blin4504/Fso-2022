50bd2ede6775e2375d5e9e2ed97f67e8afd532ce
blin4504 <bl4504@g.rit.edu>
2023-01-03 23:40:14 -0500

    token authentication in backend

--- blog/controllers/blogs.js
+++ blog/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1});
@@ -18,8 +19,11 @@ blogsRouter.get('/:id', async (request, response) => {
   
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-
-  const user = await User.findById(body.userId)
+  const user = request.user;
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({error: 'token missing or invalid' })
+  }
 
   const blog = new Blog({
     title: body.title,
@@ -36,8 +40,22 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-    await Blog.findByIdAndDelete(request.params.id)
-    response.status(204).end()
+  const token = request.token;
+  const user = request.user;
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+
+  if (!(token && decodedToken.id)) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+
+  const id = request.params.id;
+  const blog = await Blog.findById(id);
+  if (blog.user.toString() === user._id.toString()) {
+    await Blog.deleteOne({ _id: id });
+    response.sendStatus(204).end();
+  } else {
+    response.status(401).json({ error: "unauthorized operation" });
+  }
 })
 
 blogsRouter.put('/:id', async (request, response) => {
